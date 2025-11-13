9d89fe61321cc6782f00f2205c3cfe5ffffb23a5
mawbp <budiantohermawan3@gmail.com>
2025-11-13 10:00:59 +0700

    fix migrations

--- database/migrations/2025_11_13_095637_update_constraint_on_alamat_table.php
+++ database/migrations/2025_11_13_095637_update_constraint_on_alamat_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('alamat', function (Blueprint $table) {
             $table->dropForeign(['user_id']);
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
